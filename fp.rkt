1b3ccf5ddc3fd093746af3028ef9109f916980dc
couriersix <johnathan.curry@bison.howard.edu>
2017-01-27 05:11:50 -0500

    #3: fifth commit

--- fp.rkt
+++ fp.rkt
@@ -25,7 +25,7 @@
 
 (define (sum-up-numbers-general L)
   (cond ((null? L) 0)
-        ((list? L) (+ (car L) (sum-up-numbers-general (cdr L))))
+        ((list? L) (car L) (+ (sum-up-numbers-general (cdr L)) (sum-up-numbers-general (car L))))
         ((number? (car L)) (+ (car L) (sum-up-numbers-general (cdr L))))
         (else (sum-up-numbers-general(cdr L)))
    )
